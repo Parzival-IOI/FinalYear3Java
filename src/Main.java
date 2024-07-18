4448eee21ec0284198d5b0363fb55882d8bc1ad3
Kimhouy HOR <kimhouy.hor@allweb.com.kh>
2024-07-18 19:00:21 +0700

    testing

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import calculator.Calculator;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        calc.buildCalculator();
+    }
+}
